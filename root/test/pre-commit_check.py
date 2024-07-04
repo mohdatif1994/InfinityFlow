02a4a782b2864af7197343144cd57fa1e3722d50
mohd atif khan <mohdatifkhan@mohds-Laptop.local>
2024-07-05 00:42:43 +0530

    action check for pylint

--- root/test/pre-commit_check.py
+++ root/test/pre-commit_check.py
@@ -1,11 +1,19 @@
 from __future__ import annotations
 
 
+
+
 class A:
     """"""
 
     def __init__(self):
+        """"""
         pass
 
     def fun(self):
+        """"""
         return True
+
+    def decorate(self, a, b):
+        pass
+    
